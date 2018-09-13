12aa23e08300370040700d1ef741f961449083cf
Julien <fjulien819@gmail.com>
2018-09-13 11:29:50 +0200

    création formulaire page 1

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\OrderTickets;
+use AppBundle\Form\OrderTicketsType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -15,11 +18,46 @@ class DefaultController extends Controller
 
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+
+        $order = new OrderTickets;
+        $form = $this->createForm(OrderTicketsType::class, $order);
+
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST') && $form->isValid())
+        {
+
+
+            $session = $request->getSession();
+            $session->set("order", $order);
+
+
+            return $this->redirectToRoute("orderPage");
+        }
+
+
+        return $this->render('default/index.html.twig', array('form' => $form->createView(),
+        ));
     }
 
+    /**
+     * @Route("/order", name="orderPage")
+     */
+    public function billetAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        if ($session->has("order"))
+        {
+            dump($session->get("order"));
+
+            return new Response(
+                '<html><body>Page Order</body></html>'
+            );
+        }
+
+
+        return $this->redirectToRoute("homepage");
+    }
 
 }
