202e55e074c370f80f9ab8d9d2eef2b8338ed810
Julien <fjulien819@gmail.com>
2018-10-22 16:05:13 +0200

    Design email

--- src/AppBundle/Services/SendEmail/SendEmail.php
+++ src/AppBundle/Services/SendEmail/SendEmail.php
@@ -21,19 +21,39 @@ class SendEmail
     private $mailer;
     private $twig;
 
+    /**
+     * SendEmail constructor.
+     * @param \Swift_Mailer $mailer
+     * @param \Twig_Environment $twig
+     */
     public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig)
     {
         $this->mailer = $mailer;
         $this->twig = $twig;
     }
+
+    /**
+     * @param Order $order
+     * @return int
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
     public function sendTicket(Order $order)
     {
 
+
+
         $message = (new \Swift_Message(self::SUBJECT_EMAIL))
             ->setFrom(self::FROM_EMAIL)
-            ->setTo($order->getEmail())
-            ->setBody($this->twig->render('email/sendTicket.html.twig', array("order" => $order)),'text/html');
-            $this->mailer->send($message);
+            ->setTo($order->getEmail());
+
+
+        $logoWhiteSrc = $message->embed(\Swift_Image::fromPath('img/logo_white_png.png'));
+        $logoBlackSrc = $message->embed(\Swift_Image::fromPath('img/logo_black_png.png'));
+        $message->setBody($this->twig->render('email/sendTicket.html.twig', array("order" => $order, "logoWhiteSrc" => $logoWhiteSrc, "logoBlackSrc" => $logoBlackSrc)), 'text/html');
+
+        return $this->mailer->send($message);
 
     }
 
